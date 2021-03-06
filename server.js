dee4a2f2c330f47a480c31d9c3db234b34a05797
Elmer196 <elmerkobe88@gmail.com>
2021-03-06 14:29:31 -0600

    Mongo spelling error

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MOGODB_URI || "mongodb://localhost/Budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
